c3b016945fddec70051a4d4ca9d6ad439ff1645a
Dmitry Basov <dom.it.ru@yandex.ru>
2022-11-16 14:56:33 +0300

    add Task 013

---
+++ Task013/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+             или сообщает, что третьей цифры нет.*/       
+
+
+Console.Write("Задайте любое положительное число: ");
+string? myArray = Console.ReadLine();
+int numA = Convert.ToInt32(myArray);
+if (numA < 100)
+{
+    Console.Write("Третьей цифры нет.");
+}
+else
+{
+    Console.Write("Третья цифра этого числа: " + myArray[2]);
+}
