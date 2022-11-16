c8449a30fb63849b0a4ed7294ae0d29951e2c45b
Dmitry Basov <dom.it.ru@yandex.ru>
2022-11-16 13:08:24 +0300

    add Task 010

---
+++ Task010/Program.cs
@@ -0,0 +1,11 @@
+﻿ Console.WriteLine("Введите трёхзначное число: ");
+ int a = Convert.ToInt32(Console.ReadLine());
+
+ if (a/100 > 9)
+ {
+     Console.Write("Введеное число не трехзначное.");
+ }
+ else
+ {
+     Console.WriteLine($"Вторая  цифра этого числа: {(a%100-a%10)/10}");
+ }
